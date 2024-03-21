cdf215e4cb35a68621efacd0a7debcd5cacd27b7
Nadi2894 <nadirabegimnadyrbekova@gmail.com>
2024-03-21 13:04:40 +0100

    Added new components, html, bootstrap, axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App container">
+        <Weather />
+        <footer>
+          This page was made by Nadira-Begim and is {""}
+          <a href="https://github.com/Nadi2894/my-react-weather-app">
+            open-sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
